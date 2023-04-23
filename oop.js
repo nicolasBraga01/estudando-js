081263121ef9df665c9f25ed6c5f7939e48236bd
nicolas <nicolasbr921@gmail.com>
2023-04-23 13:20:32 -0300

    obj

---
+++ oop.js
@@ -0,0 +1,17 @@
+
+// Objetos e metódos
+
+const livro = {
+    paginas: 600,
+    autor: 'Nicolas',
+    Capitulos : {
+        capitulo1: 'Inicio',
+        capitulo2: 'Recomeço' 
+    },
+    menSagem : function (){
+        console.log('Carregando...')
+    }
+}
+
+livro.menSagem()
+livro.autor
