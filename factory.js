dbee3257bf6cb152788345aeae4b0aa2d9598909
nicolas <nicolasbr921@gmail.com>
2023-04-23 15:14:33 -0300

    factory

---
+++ factory.js
@@ -0,0 +1,16 @@
+
+// Cria e organiza objetos
+
+function createBook(title, author, pages, year){
+    const book ={
+        bookTitle : title,
+        bookAuthor: author,
+        bookPages: pages,
+        bookYear: year,
+    }
+    return book
+    // finaliza e retornas os valores para o book
+}
+
+let book1 = createBook('Iracema','José de Alencar', 90, 1884);
+let book2 = createBook('Hamlet', 'William Shakespeare', 149, 1599);
