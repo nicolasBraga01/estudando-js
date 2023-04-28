55aedc54924a0be3a7ae8d9afa70fb2ff20ce8eb
nicolas <nicolasbr921@gmail.com>
2023-04-27 21:09:43 -0300

    array

---
+++ arrayMethod.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+const frutas = ['Maça', 'Pera', 'Uva', 'Banana']
+console.log(frutas.indexOf('Pêssego'))
+
+// Caso contenha o parâmetro passado ele informe o índice que ele está
+
+// Resposta -1 pq não existe na array
+
+
+const movies = [
+    {id: 1, movieName:'Tarzan'},
+    {id: 2, movieName:'Futurama'},
+    {id: 3, movieName:'Zumbilândia'},
+    {id: 4, movieName:'De Volta para o Futuro'}
+]
+
+console.log(movies.find(function(movie){
+    return movie.movieName === 'Django Livre'
+}))
+
+// undefined pq ñ existe na array
