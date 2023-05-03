4cc6692b7a4573085b91357f6e39530ccbc83c86
nicolas <nicolasbr921@gmail.com>
2023-05-02 21:11:20 -0300

    functionArgument

---
+++ functionArgument.js
@@ -0,0 +1,14 @@
+
+// Passando argumentos na função 
+
+function price(){
+    let total = 0;
+
+    // n preciso declarar por exemplo price(a, b, c, d)
+    for (let value of arguments)
+    
+    total += value
+    return total
+}
+
+console.log(price(10,40,30,50,40,10))
