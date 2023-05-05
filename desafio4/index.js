acb3bcca71958243812b17e559b5206cf32501c1
nicolas <nicolasbr921@gmail.com>
2023-05-05 18:29:57 -0300

    Muda Cor de fundo com um click

--- desafio4/index.js
+++ desafio4/index.js
@@ -1,4 +1,23 @@
 
-const colors = ['Red', 'purple']
 
-// continua...
+const colors = ['Green', 'Yellow', 'Purple', 'Blue', 'Red']
+const btn = document.getElementById('btn')
+
+// retorna o primeiro seletor correspondente
+const color = document.querySelector('#cor')
+
+// "escuta o html" e executa
+btn.addEventListener('click', function() {
+
+    const randomColor = getRandomColor()
+
+    // mexe no css
+    document.body.style.background = colors[randomColor]
+    color.textContent = colors[randomColor]
+
+})
+
+function getRandomColor() {
+    // retornar números inteiros
+    return Math.floor(Math.random() * colors.length)
+}
