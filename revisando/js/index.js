27785c0055d3ab622b239c01105e2fad257ae7c0
nicolas <nicolasbr921@gmail.com>
2023-05-17 22:49:13 -0300

    apanhando

--- revisando/js/index.js
+++ revisando/js/index.js
@@ -1,1 +1,19 @@
 // Cenas para os próximos cápitulos
+
+function principal() {
+  // console.log('teste')
+  let pessoas = [];
+  let pessoa = {
+    nome: document.getElementById("name").value,
+    senha: document.getElementById("senha").value,
+  };
+  function armazena() {
+    addEventListener("submit", function () {
+      pessoas.push(pessoa.nome);
+      pessoas.push(pessoa.senha);
+    });
+  } 
+ 
+}
+let resultado = document.getElementById('output').innerHTML = "";
+// falha em repassar os dados , ou talvez armazenar
