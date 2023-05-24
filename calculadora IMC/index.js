db61737579ef2e771d61e8d2ac1ed238ad4872bf
nicolas <nicolasbr921@gmail.com>
2023-05-24 20:52:06 -0300

    em fase de testes

--- calculadora IMC/index.js
+++ calculadora IMC/index.js
@@ -9,16 +9,20 @@ form.addEventListener("submit", function (e) {
   //pega os valores e transforma em número
   const peso = Number(document.querySelector("#peso").value);
   const altura = Number(document.querySelector("#altura").value);
-  console.log(peso,altura)
+  console.log(peso, altura);
 
-  if (!peso){
-    setResultado('peso invalido', false);
+  //caso peso não seja um número "faça isso"
+  if (!peso) {
+    setResultado("peso invalido", false);
     return;
   }
-  if (!altura){
-    setResultado('altura invalida', false);
+   //caso altura não seja um número "faça isso"
+  if (!altura) {
+    setResultado("altura invalida", false);
     return;
   }
+  const calculaImc = parseFloat(peso / altura **2 );
+  document.querySelector('#resultado').innerHTML = ' Seu IMC é ' + calculaImc.toFixed(2);
 });
 
 // automatiza a criação de paragrafos
@@ -28,7 +32,7 @@ function fazP() {
 }
 function setResultado(mensagem, isVAlid) {
   const resultado = document.querySelector("#resultado");
-  resultado.innerHTML = '';
+  resultado.innerHTML = "";
   // cria um elemento do html no javascript
   //   const p = document.createElement("p");
 
@@ -37,7 +41,7 @@ function setResultado(mensagem, isVAlid) {
   // adiciona uma classe no elemento que eu criei, oq me dar a possibilidade de formatar pelo css;
   //   p.classList.add("p-resultado");
 
-     p.innerHTML = mensagem;
+  p.innerHTML = mensagem;
   //   so funciona se tiver essa último parte pois ele diz que o p entrará na div resultado (parte importante)
   resultado.appendChild(p);
 }
