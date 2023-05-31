32a2333c3ed5b55fda196347499b88b92813692f
nicolas <nicolasbr921@gmail.com>
2023-05-31 19:21:32 -0300

    atualizando conteudo

--- switchCase.js
+++ switchCase.js
@@ -1,6 +1,6 @@
 // switchCase= verifica e executa as intruções
 
-let frutaPredileta = 'banana';
+ let frutaPredileta = 'banana';
 
 switch (frutaPredileta) {
   case 'pera':
@@ -15,3 +15,26 @@ switch (frutaPredileta) {
   default:
     console.log('Passou foi longe');
 }
+
+// se eu colocar dentro de uma function eu posso substituir o break por return
+
+// function fruta(frutaPredileta){
+//   let frutaPrediletaText;
+ 
+//   switch (frutaPredileta) {
+//     case 'pera':
+//       frutaPrediletaText = 'acertou';
+//      return frutaPrediletaText;
+//     case 'banana':
+//       frutaPrediletaText = 'Não foi dessa vez';
+//       return frutaPrediletaText;
+//     case 'maça':
+//       frutaPrediletaText = 'Quase lá';
+//       return frutaPrediletaText;
+//     default:
+//       frutaPrediletaText = 'passou foi longe';
+//   }
+
+// }
+
+// fruta('banana');
